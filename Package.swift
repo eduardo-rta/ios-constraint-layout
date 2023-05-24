01e72f066742910426b7e05535db22894fd4d2e0
Eduardo Revuelta <eduardo.revuelta@gmail.com>
2023-05-23 20:13:44 -0400

    Fix

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ConstraintLayout",
     platforms: [
-        .iOS(.v13),
-        .watchOS(.v5)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -24,9 +23,14 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ConstraintLayout",
-            dependencies: []),
+            dependencies: [],
+            linkerSettings: [
+                .linkedFramework("UIKit")
+            ]
+        ),
         .testTarget(
             name: "ConstraintLayoutTests",
             dependencies: ["ConstraintLayout"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
